dcc1990bec4fc4f5d1d5bb12467ae2c1401199a0
Benjymack <newzealandbenjy@gmail.com>
2021-08-19 16:38:24 +1200

    Hello World

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+############
+#
+# Video Tracker
+# Author: Benjy Smith
+# Allows users to track videos
+#
+############
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtWidgets import QApplication, QLabel
 
+app = QApplication([])
+label = QLabel('Hello World!')
+label.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.exec_()
