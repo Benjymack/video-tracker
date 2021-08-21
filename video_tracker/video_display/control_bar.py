5e81ccc6c5a056434fdbcfdba41c42ac19b3a300
Benjymack <newzealandbenjy@gmail.com>
2021-08-22 11:47:01 +1200

    Started scrubber and frame increment/decrement buttons

--- video_tracker/video_display/control_bar.py
+++ video_tracker/video_display/control_bar.py
@@ -1,7 +1,7 @@
 # Imports
-from PyQt5.QtCore import QSize
+from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QWidget, QPushButton, QStyle, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QStyle, QHBoxLayout, QSlider, QSizePolicy
 
 
 # Constants
@@ -18,16 +18,41 @@ class ControlBar(QWidget):
 
         # Play/pause button
         self._play_pause_button = QPushButton()
-        self._play_pause_button.setEnabled(False)  # Before the user has opened a video, disable the button
         self._play_pause_button.setFixedHeight(24)  # TODO: What is this for?
         self._play_pause_button.setIconSize(ICON_SIZE)
         self._play_pause_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
 
+        # Scrubber
+        self._scrubber = QSlider(Qt.Horizontal)
+        self._scrubber.setRange(0, 0)
+
+        # Frame decrement button
+        self._frame_decrement_button = QPushButton()
+
+        # Frame increment button
+        self._frame_increment_button = QPushButton()
+
         # TODO: Trial the layout of the buttons
         self._layout = QHBoxLayout()
+        self._layout.addWidget(self._frame_decrement_button)
         self._layout.addWidget(self._play_pause_button)
+        self._layout.addWidget(self._frame_increment_button)
+        self._layout.addWidget(self._scrubber)
 
         self.setLayout(self._layout)
+        
+        self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.Fixed)
+
+        self._buttons = (
+            self._play_pause_button,
+            self._frame_decrement_button,
+            self._frame_increment_button,
+        )
 
     def register_controller(self, controller):
         self._play_pause_button.clicked.connect(controller.play_pause_toggle)  # TODO: Add function to controller
+        self._scrubber.sliderMoved.connect(controller.position_changed)
+
+    def set_enabled_buttons(self, are_enabled):
+        for button in self._buttons:
+            button.setEnabled(are_enabled)
