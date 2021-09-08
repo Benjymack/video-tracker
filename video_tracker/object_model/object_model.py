a25406b2e54cb53b0446f2a5005650b3341b0179
Benjymack <newzealandbenjy@gmail.com>
2021-09-08 17:59:40 +1200

    Started ObjectModel

---
+++ video_tracker/object_model/object_model.py
@@ -0,0 +1,13 @@
+# Classes
+class ObjectModel:
+    # Stores information about an object
+    def __init__(self, points=None):
+        if points is None:
+            points = {}
+        self._points = points
+
+    def add_point(self, x, y, frame):
+        self._points[frame] = (x, y)
+
+    def get_last_n_points(self, n):
+        return dict(sorted(self._points.items())[-n:])
